00556bd836a51fb4a95fc278b212492c04856cef
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-01-23 16:03:39 -0300

    update keep_alive.js

--- src/lib/keep_alive.js
+++ src/lib/keep_alive.js
@@ -1,8 +1,6 @@
-//var http = require('http');
+var http = require('http');
 require("fix-esm").register();
-/*
 http.createServer(function (req, res) {
 res.write("Funcionando!");
 res.end();
-}).listen(8000);
-*/
+}).listen(3000);
