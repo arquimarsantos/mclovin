8fb9a5f06164d49f19fe1b1b99317be0d44badac
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-01-23 17:15:35 -0300

    update keep_alive.js

--- src/lib/keep_alive.js
+++ src/lib/keep_alive.js
@@ -1,6 +1,14 @@
-var http = require('http');
+//var http = require('http');
 require("fix-esm").register();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) => {
+res.send('online!')
+})
+/*
 http.createServer(function (req, res) {
 res.write("Funcionando!");
 res.end();
-}).listen(3000);
+}).listen(3306);
+*/
