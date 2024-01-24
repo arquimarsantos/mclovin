9700fec7baf19c3b1f5098d5a22714e363a4d26d
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-01-23 21:52:20 -0300

    add files via upload

--- src/lib/keep_alive.js
+++ src/lib/keep_alive.js
@@ -1,12 +1,12 @@
 //var http = require('http');
 require("fix-esm").register();
+/*
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
 res.send('online!')
 })
-/*
 http.createServer(function (req, res) {
 res.write("Funcionando!");
 res.end();
