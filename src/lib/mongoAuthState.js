04eed4bfe861c373980c29d701cc490306e8f2b7
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-01-30 06:24:51 -0300

    update mongoAuthState.js

--- src/lib/mongoAuthState.js
+++ src/lib/mongoAuthState.js
@@ -1,36 +1,12 @@
 const { MongoClient } = require('mongodb');
 const { proto } = require("@whiskeysockets/baileys/WAProto");
-const {
-  Curve,
-  signedKeyPair,
-} = require("@whiskeysockets/baileys/lib/Utils/crypto");
-const {
-  generateRegistrationId,
-} = require("@whiskeysockets/baileys/lib/Utils/generics");
-const { randomBytes } = require("crypto");
+const { initAuthCreds } = require('@whiskeysockets/baileys/lib/Utils/auth-utils')
 
 const MongoDBAuthConfig = {
   mongodbUri: "",
   databaseName: "",
   collectionName: "",
   sessionId: ""
-};
-
-const initAuthCreds = () => {
-  const identityKey = Curve.generateKeyPair();
-  return {
-    noiseKey: Curve.generateKeyPair(),
-    signedIdentityKey: identityKey,
-    signedPreKey: signedKeyPair(identityKey, 1),
-    registrationId: generateRegistrationId(),
-    advSecretKey: randomBytes(32).toString("base64"),
-    processedHistoryMessages: [],
-    nextPreKeyId: 1,
-    firstUnuploadedPreKeyId: 1,
-    accountSettings: {
-      unarchiveChats: false,
-    },
-  };
 };
 
 const BufferJSON = {
@@ -108,7 +84,7 @@ const useMongoDBAuthState = async(config) => {
       await collection.deleteOne({ _id: id });
     } catch (_a) {}
   };
-  const creds = (await readData("creds")) || (0, initAuthCreds)();
+  const creds = (await readData("creds")) || initAuthCreds();
   return {
     state: {
       creds,
