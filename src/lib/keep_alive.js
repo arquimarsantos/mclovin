4064b6c8c8183ea8fa8acea5e43369e92568c80b
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-03-11 12:22:28 -0300

    add files via upload

--- src/lib/keep_alive.js
+++ src/lib/keep_alive.js
@@ -4,8 +4,7 @@ const express = require('express')
 const app = express()
 const { SocksProxyAgent } = require('socks-proxy-agent')
 // const agent = new ProxyAgent()
-162.210.192.136
-const agent = new SocksProxyAgent('socks5://74.119.144.60:4145')
+const agent = new SocksProxyAgent('socks5://162.210.192.136:30334')
 require("fix-esm").register();
 // Proxies aprovados
 // 74.119.144.60:4145
