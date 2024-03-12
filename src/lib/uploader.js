c9170f8efb95eaab347b61f0ed2fa3cbf6bd2af8
Arquimar <49088324+arquimarsantos@users.noreply.github.com>
2024-03-12 15:13:40 -0300

    add files via upload

--- src/lib/uploader.js
+++ src/lib/uploader.js
@@ -1,6 +1,9 @@
-let axios = require('axios')
-let BodyForm = require('form-data')
-let fs = require('fs')
+const axios = require('axios')
+const fetch = require('node-fetch')
+const BodyForm = require('form-data')
+const { FormData, Blob } = require('formdata-node')
+const fs = require('fs')
+const { JSDOM } = require('jsdom')
 
 
 
@@ -43,4 +46,36 @@ async function UploadFileUgu (input) {
 	})
 }
 
-module.exports = { TelegraPh, UploadFileUgu }
+
+async function webp2mp4(source) {
+    let form = new FormData()
+    let isUrl = typeof source === 'string' && /https?:\/\//.test(source)
+    const blob = !isUrl && new Blob([source.toArrayBuffer()])
+    form.append('new-image-url', isUrl ? blob : '')
+    form.append('new-image', isUrl ? '' : blob, 'image.webp')
+    let res = await fetch('https://ezgif.com/webp-to-mp4', {
+        method: 'POST',
+        body: form
+    })
+    let html = await res.text()
+    let {
+        document
+    } = new JSDOM(html).window
+    let form2 = new FormData()
+    let obj = {}
+    for (let input of document.querySelectorAll('form input[name]')) {
+        obj[input.name] = input.value
+        form2.append(input.name, input.value)
+    }
+    let res2 = await fetch('https://ezgif.com/webp-to-mp4/' + obj.file, {
+        method: 'POST',
+        body: form2
+    })
+    let html2 = await res2.text()
+    let {
+        document: document2
+    } = new JSDOM(html2).window
+    return new URL(document2.querySelector('div#output > p.outfile > video > source').src, res2.url).toString()
+}
+
+module.exports = { TelegraPh, UploadFileUgu, webp2mp4 }
